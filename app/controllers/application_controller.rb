094ef41de7c6ab66f5fbc4a8a7684b31cbeb8032
space pudim <rogi@skylittlesystem.org>
2019-11-18 11:49:42 +0000

    Fix applications controller logic in wrong file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,2 @@
 class ApplicationController < ActionController::Base
-  def index
-    @applications = Application.all
-  end
-
-  def show
-    @application = Application.find(params[:id])
-  end
 end
