cbd3d063ed4495d852b490fc239e9c8dc021b86a
space pudim <rogi@skylittlesystem.org>
2019-11-22 14:31:51 +0000

    Pipe document files from API as they are read

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,6 +1,21 @@
 class DocumentsController < ApplicationController
   def show
     @document = Document.find(params[:id])
-    send_file(@document.file_stream, filename: @document.original_name)
+
+    # pipe the file data as we read it from the API
+    uri = URI.parse(@document.download_url)
+    Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
+      request = Net::HTTP::Get.new(uri)
+
+      # use same headers as an SmpaResource
+      SmpaResource.headers.each {|k, v| request[k] = v }
+
+      http.request(request) do |response|
+        headers["Content-Type"]   = response["Content-Type"]
+        headers["Content-Length"] = response["Content-Length"]
+        headers["Content-Disposition"] = "attachment; filename=#{@document.original_name}"
+        self.response_body = response.read_body
+      end
+    end
   end
 end
