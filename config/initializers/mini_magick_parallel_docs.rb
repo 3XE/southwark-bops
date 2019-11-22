21a5702d7e9335cc09e4f2618cde703ed6df1dc7
space pudim <rogi@skylittlesystem.org>
2019-11-22 20:53:18 +0000

    Hack mini_magick crash with doc files in parallel
    This is a hack around a bug in `identify`, from ImageMagick, that causes
    a crash when two Microsoft Office files, like two different Word
    documents, are processed at the same time.
    
    If the files are processed one at a time, it works correctly, but if
    done at the same time, it crashes.
    
    To fix, MiniMagick::Image.read is patched to wait on a global Mutex when
    called for matching file extensions.

---
+++ config/initializers/mini_magick_parallel_docs.rb
@@ -0,0 +1,19 @@
+# HACK: around crash caused by running `identify` on multiple microsoft office
+# files in parallel
+$mini_magick_mutex = Mutex.new
+
+module MiniMagick
+  class Image
+    class <<self
+      alias_method :read_orig, :read
+
+      def read(stream, ext = nil)
+        if /(doc|xls|ppt)/ =~ ext
+          $mini_magick_mutex.synchronize { read_orig(stream, ext) }
+        else
+          read_orig(stream, ext)
+        end
+      end
+    end
+  end
+end
