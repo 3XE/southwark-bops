c2008fcf42fd625c6e122157c4c01d43f5af8a96
space pudim <rogi@skylittlesystem.org>
2019-11-25 15:25:07 +0000

    Add proposed/existing types to document listing

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,4 +1,7 @@
 class Document < SmpaResource
+  has_many :document_types_existing, class_name: "Document::Type"
+  has_many :document_types_proposed, class_name: "Document::Type"
+
   def self.download_url(id)
     URI.join(site, prefix, "documents/#{id}/download").to_s
   end
---
+++ app/models/document/type.rb
@@ -0,0 +1,5 @@
+class Document::Type < SmpaResource
+  schema do
+    string :name
+  end
+end
