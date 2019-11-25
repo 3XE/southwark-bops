abc77cea7a7d4bb7e2afec8add4e733144ccc163
space pudim <rogi@skylittlesystem.org>
2019-11-25 14:43:29 +0000

    Cache processed thumbnails in memory

--- app/controllers/documents/thumbnails_controller.rb
+++ app/controllers/documents/thumbnails_controller.rb
@@ -2,14 +2,18 @@ class Documents::ThumbnailsController < ApplicationController
   def show
     @document = Document.find(params[:document_id])
 
-    image = MiniMagick::Image.open(
-      @document.download_url,
-      File.extname(@document.original_name),
-      SmpaResource.headers
-    )
+    @blob = $cache.fetch("/documents/#{params[:document_id]}/thumbnail.png") do
+      image = MiniMagick::Image.open(
+        @document.download_url,
+        File.extname(@document.original_name),
+        SmpaResource.headers
+      )
 
-    image.resize "128x128"
-    image.format "png"
-    send_file image.path, filename: "#{@document.id}.png"
+      image.resize "128x128"
+      image.format "png"
+      image.to_blob
+    end
+
+    send_data @blob, filename: "#{@document.id}_thumbnail.png"
   end
 end
